70c481b69fd1a58518358c5a4bc7d4d9663bcc22
Jesse Cooper <jesse@iterate.co>
2025-10-24 16:46:32 -0700

    build

--- docs/assets/secp256k1-ClZ6g-y6.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-KVPoB0F8.js";export{e as secp256k1};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vy-monitor/',
   build: {
     outDir: 'docs'
   },
