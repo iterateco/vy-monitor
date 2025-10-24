2363f455423fddf14bf86dbb643c9d89bd8e694a
Jesse Cooper <jesse@iterate.co>
2025-10-24 16:38:27 -0700

    build

---
+++ docs/assets/secp256k1-ClZ6g-y6.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-KVPoB0F8.js";export{e as secp256k1};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     react({
       babel: {
